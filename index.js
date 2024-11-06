8ec5c18aabdce47cf2e50f09a672c1bcb5f98998
Ahmad0301 <sehroz3040@gmail.com>
2024-11-06 20:10:12 +0500

    read file

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const express=require('express')
 const fs=require('fs')
 const app = express()
 app.use(express.json())
@@ -15,12 +16,25 @@ app.post('/book',(req,resp)=>{
         {
             resp.status(201).send('Book added successfully')
         }
+    })    
+})
 
 
+app.get('/read',(req,resp)=>{
+    fs.readFile('new.json',(err,data)=>
+        {
+        if (err) {
+            console.log(err)
+            resp.status(500).send('Error reading file')
+        }
+        else 
+        {
+            resp.status(201).send('new.json')       }
+            
+        })
     })
-     
-})
 
-app.listen(3000,(req,resp)=>{
-    console.log('Server is running on port 3000')
-})
+
+    app.listen(3000,(req,resp)=>{
+        console.log('Server is running on port 3000')
+    })
